df2f3da4e30c41e129f7fadffb8023cec3a9ec04
Hanslemkimeng <116292516+Richmindvybez@users.noreply.github.com>
2023-08-11 18:23:08 +0100

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,87 @@
+
+// testimonial script ==================================
+let tablinks = document.getElementsByClassName('tab-imglink');
+let tabcontents = document.getElementsByClassName('tab-contents');
+
+function Opentab(tabname){
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove('active-tab');
+  }
+  
+  for(tablink of tablinks){
+    tablink.classList.remove('active-tablink');
+  }
+  
+  event.target.classList.add('active-tablink');
+  document.getElementById(tabname).classList.add('active-tab');
+}
+
+
+
+// menu toggle ======================================================================
+
+let menuI = document.querySelector('.fa-bars');
+let navs = document.querySelector('.navs');
+
+menuI.addEventListener('click', () => {
+  menuI.classList.toggle('fa-xmark');
+  navs.classList.toggle('Active-menu');
+})
+
+
+
+// carousel ====================================================================
+$(".carousel").owlCarousel({
+  margin: 30,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+    },
+    1100: {
+      items: 4,
+      nav: false,
+    },
+  }
+})
+
+// testimonial slider ==============================
+// $(".profiles").owlCarousel({
+//   margin: 30,
+//   loop: true,
+//   autoplay: true,
+//   autoplayTimeout: 5000,
+//   autoplayHoverPause: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//       nav: false,
+//     },
+    
+//     600: {
+//       items: 2,
+//       nav: false,
+//     },
+//     1000: {
+//       items: 3,
+//       nav: false,
+//     },
+//     1100: {
+//       items: 4,
+//       nav: false,
+//     },
+//   }
+// })
