6f35c98e4f34ca63d62c52dbd5865dc3f1d4f002
Hanslemkimeng <116292516+Richmindvybez@users.noreply.github.com>
2023-08-11 23:08:25 +0100

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,25 +1,25 @@
 
 // testimonial script ==================================
-let tablinks = document.getElementsByClassName('tab-imglink');
-let tabcontents = document.getElementsByClassName('tab-contents');
+// let tablinks = document.getElementsByClassName('tab-imglink');
+// let tabcontents = document.getElementsByClassName('tab-contents');
 
-function Opentab(tabname){
-  for(tabcontent of tabcontents){
-    tabcontent.classList.remove('active-tab');
-  }
+// function Opentab(tabname){
+//   for(tabcontent of tabcontents){
+//     tabcontent.classList.remove('active-tab');
+//   }
   
-  for(tablink of tablinks){
-    tablink.classList.remove('active-tablink');
-  }
+//   for(tablink of tablinks){
+//     tablink.classList.remove('active-tablink');
+//   }
   
-  event.target.classList.add('active-tablink');
-  document.getElementById(tabname).classList.add('active-tab');
-}
+//   event.target.classList.add('active-tablink');
+//   document.getElementById(tabname).classList.add('active-tab');
+// }
 
 
 
-// menu toggle ======================================================================
 
+// menu icon toggle 
 let menuI = document.querySelector('.fa-bars');
 let navs = document.querySelector('.navs');
 
@@ -28,9 +28,10 @@ menuI.addEventListener('click', () => {
   navs.classList.toggle('Active-menu');
 })
 
+// home background =========================================================
 
 
-// carousel ====================================================================
+// store carousel ====================================================================
 $(".carousel").owlCarousel({
   margin: 30,
   loop: true,
@@ -58,30 +59,30 @@ $(".carousel").owlCarousel({
   }
 })
 
-// testimonial slider ==============================
-// $(".profiles").owlCarousel({
-//   margin: 30,
-//   loop: true,
-//   autoplay: true,
-//   autoplayTimeout: 5000,
-//   autoplayHoverPause: true,
-//   responsive: {
-//     0: {
-//       items: 1,
-//       nav: false,
-//     },
+// testimonial carousel ==============================
+$(".profiles").owlCarousel({
+  margin: 10,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
     
-//     600: {
-//       items: 2,
-//       nav: false,
-//     },
-//     1000: {
-//       items: 3,
-//       nav: false,
-//     },
-//     1100: {
-//       items: 4,
-//       nav: false,
-//     },
-//   }
-// })
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 4,
+      nav: false,
+    },
+    1100: {
+      items: 5,
+      nav: false,
+    },
+  }
+})
